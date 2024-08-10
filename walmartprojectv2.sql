ef15228ef7190816c41f6335afac825301a03f7e
Hareg <haregwoldeg@gmail.com>
2024-08-09 22:01:38 -0400

    Add files via upload

--- walmartprojectv2.sql
+++ walmartprojectv2.sql
@@ -7,7 +7,7 @@ select ordertime , (CASE
     ELSE'Night' END) as 'workshift' 
     from salesdata_o;
 
-    -- ============Records from specific date intervals===================== 
+    -- ==========Return the sales record b/n jan and march for a specific shift== 
                 -- use cte to write clear coode --
     -- ---------------------------------------------------------------------
 WITH salesrecord as (
@@ -19,9 +19,18 @@ select *,
     from salesdata_o
 )
 select * from salesrecord 
-where timeordered='Morning' and orderDate between '2019-01-05' and '2019-03-31';
+where workshift= 'Morning' and orderDate between '2019-01-05' and '2019-03-31';
 
--- =====================productivity of each city by shift ================
+-- ===================== Which product line had the largest revenue====
+                --Revenue = sum of total sales 
+---------------------------------------------------------------------------
+      SELECT Product_line, sum(total) as revenue
+      from salesdata_o  
+      GROUP BY Product_line
+      ORDER BY revenue DESC;        
+        -- ans: Food and beverages had the largest revenue, $56,144
+
+-- =====================Productivity of each city by shift ================
                 -- for each city check which shift gets more order 
 -- ------------------------------------------------------------------------
                         
@@ -64,8 +73,16 @@ ORDER BY count(*) desc;
                 --3. Morning sell: Home and lifestyle prdt is the highest
                      -- Health and beauty being the least selling
 
+-- =====================City with largest Revenue  =======================================
+                -- ans: Napyitaw city  had the largest revenue, $110,568 
+-- ------------------------------------------------------------------------
+select city, sum(total) as Revenue
+from salesdata_o
+GROUP BY City
+order by sum(total) desc;
+
 -- =====================payment methods =======================================
-                -- use view table salesrecordbyshift 
+                -- 3 known payment methods recorded
 -- ------------------------------------------------------------------------
 SELECT Payment, count(*) as pymtcount
 from salesdata_o
@@ -92,13 +109,50 @@ ORDER BY branch,count(*) desc
 -- =====================Which month is productive ============================
                 
 -- ------------------------------------------------------------------------
-select  YEAR(orderDate) AS orderyear, monthname(orderDate) as ordermonth, count(*) as ordercount
+select  YEAR(orderDate) AS orderyear, monthname(orderDate) as ordermonth,round(sum(total),2) as totaSale
 from salesdata_o
 group by YEAR(orderDate), monthname(orderDate)
-        --- ans: Month January has highest order , 352 orders 
+        --- ans:January has highest sale-->  $116,292 
 
 -- list order months in record --- 
 select distinct monthname(orderdate) from salesdata_o;
+
+-- ----------------------------------------------------------------------------
+-- ============Which month has the largest Revenue &  COGS ,cost of goods sold?===
+-- ----------------------------------------------------------------------------
+
+select monthname(orderDate) as MonthName,sum(total) as Revenue,sum(COGS) as cogs_sold
+from salesdata_o
+GROUP BY monthname(orderDate)
+ORDER BY Revenue DESC ;
+   -- ans : Jan had  the highest Revenue,$116,292 &
+   -- February has the highest COGS, $110,754
+
+-- ------------------------------------------------------------------------
+--===============which branch sold more product than average pdt sold?=====
+            -- used view table to create  ----
+ -- -----------------------------------------------------------------------
+ create view sum_pdtsold as
+    select branch,sum(Quantity) qntysold
+    from salesdata_o
+    group by branch;
+ 
+SELECT branch,sum(Quantity) as qnty
+FROM salesdata_o
+GROUP BY branch
+HAVING qnty > (select avg(qntysold) from sum_pdtsold);
+ 
+
+-- --------------------------------------------------------------------------
+-- =============label product line as good,and bad based on avg sales========
+                -- use case statment --
+-- --------------------------------------------------------------------------
+ SELECT date(orderdate) as orderdate, product_line,total as sale,(
+ CASE 
+ WHEN total > (SELECT AVG(total) AS avgsale from salesdata_o) THEN 'Good'
+ ELSE 'Bad' END) AS pdt_status
+ from salesdata_o;
+
 
 -- =========== create stored procudure to retrieve specific year data========
         -- This can help to create visuals faster    
@@ -111,7 +165,7 @@ select *,
     WHEN ordertime BETWEEN "00:00:00" AND "12:00:00" THEN 'Morning'
     WHEN ordertime BETWEEN "12:01:00" AND "16:00:00" THEN 'Afternoon'
     ELSE'Night' END) as 'workshift' 
-    from salesdata_o where year(orderDate)=ord_year;
+    from salesdata_o where year(orderDate)= ord_year;
     end//
     delimiter ; 
     -- stored procedure named salesrecordwithshift1 created above with year as param.
